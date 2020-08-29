639aaccbafeb5bf956ef316ea3a053b31dd33d68
Max Eineder <max.eineder@googlemail.com>
2020-08-29 23:20:48 +0200

    Add constraints

--- Models/Make.cs
+++ Models/Make.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace vega.Models
 {
+
+    [Table("Makes")]
     public class Make
     {
         public int Id { get; set; }
+      
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
         public ICollection<Model> Models { get; } = new Collection<Model>();
     }
