0269298dcc223313e62c2d065d098b606da7b351
Max Eineder <max.eineder@googlemail.com>
2020-09-10 21:31:13 +0200

    Add feature entity

---
+++ Migrations/20200910121932_SeedFeature.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace vega.Migrations
+{
+    public partial class SeedFeature : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO FEATURES (Name) VALUES ('Feature1')");
+            migrationBuilder.Sql("INSERT INTO FEATURES (Name) VALUES ('Feature2')");
+            migrationBuilder.Sql("INSERT INTO FEATURES (Name) VALUES ('Feature3')");
+            migrationBuilder.Sql("INSERT INTO FEATURES (Name) VALUES ('Feature4')");
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM FEATURES WHERE Name = 'Feature1'");
+            migrationBuilder.Sql("DELETE FROM FEATURES WHERE Name = 'Feature2'");
+            migrationBuilder.Sql("DELETE FROM FEATURES WHERE Name = 'Feature3'");
+            migrationBuilder.Sql("DELETE FROM FEATURES WHERE Name = 'Feature4'");
+        }
+    }
+}
