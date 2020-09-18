04ebc2dac3fcbe5a0eda06f7696c2afd30ee8a11
Max Eineder <max.eineder@googlemail.com>
2020-09-18 21:39:11 +0200

    Add route and link for new vehicle

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -28,6 +28,7 @@ import { VehicleFormComponent } from './vehicle-form/vehicle-form.component';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'vehicles/new', component: VehicleFormComponent}
     ])
   ],
   providers: [],
