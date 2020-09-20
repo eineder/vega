e09fe1b08c067397a9f00016c8e58bb67a57f788
Max Eineder <max.eineder@googlemail.com>
2020-09-20 23:25:24 +0200

    Bind vehicle form to service data

--- ClientApp/src/app/services/make.service.ts
+++ ClientApp/src/app/services/make.service.ts
@@ -1,17 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import 'rxjs/add/operator/map';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MakeService {
 
-  constructor(private http: Http) { }
+  constructor(private http: HttpClient) { }
 
   getMakes() {
-    return this.http.get('/api/makes')
-               .map(res => res.json());
+    return this.http.get<any[]>('/api/makes')
+               .pipe(map(res => res));
   }
 
 }
--- ClientApp/src/app/vehicle-form/vehicle-form.component.ts
+++ ClientApp/src/app/vehicle-form/vehicle-form.component.ts
@@ -1,3 +1,4 @@
+import { MakeService } from './../services/make.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VehicleFormComponent implements OnInit {
 
-  constructor() { }
+  makes: any[];
+  vehicle: any = {};
+  models: any[];
+
+  constructor(private makeService: MakeService) { }
 
   ngOnInit() {
+    this.makeService.getMakes().subscribe(makes =>
+          this.makes = makes);
   }
 
+  onMakeChange(){
+   const selectedMake = this.makes.find(m => m.id == this.vehicle.make);
+   this.models = selectedMake.models;
+  }
 }
